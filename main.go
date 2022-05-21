469a516f0dfd505a7c32c9c5d7ecd29e1dde4db8
kaitoyama <uitakato@gmail.com>
2022-05-21 19:46:27 +0900

    追加削除実装

--- main.go
+++ main.go
@@ -25,18 +25,49 @@ func main() {
 		log.Fatalf("Cannot Connect to Database: %s", err)
 	}
 
-	input_city := os.Args[1]
-
 	fmt.Println("Connected!")
 	var city City
+	cities := []City{}
 
-	query := "SELECT * FROM city WHERE Name=?"
+	query_city := "SELECT * FROM city WHERE Name=?"
+	query_cities := "SELECT * FROM city WHERE CountryCode=?"
+	countryCity := "INSERT INTO city (CountryCode, Name) VALUES (?, ?)"
+	delete_city := "DELETE FROM city WHERE Name=?"
 
-	if err := db.Get(&city, query, input_city); errors.Is(err, sql.ErrNoRows) {
-		log.Printf("no such city Name = %s", input_city)
-	} else if err != nil {
-		log.Fatalf("DB Error: %s", err)
+	switch os.Args[1] {
+	case "city":
+		input_city := os.Args[2]
+		if err := db.Get(&city, query_city, input_city); errors.Is(err, sql.ErrNoRows) {
+			log.Printf("no such city Name = %s", input_city)
+		} else if err != nil {
+			log.Fatalf("DB Error: %s", err)
+		}
+
+		fmt.Printf(input_city+"の人口は%d人です\n", city.Population)
+
+	case "cities":
+		input_country := os.Args[2]
+		if err := db.Select(&cities, query_cities, input_country); errors.Is(err, sql.ErrNoRows) {
+			log.Printf("no such country Name = %s", input_country)
+		} else if err != nil {
+			log.Fatalf("DB Error: %s", err)
+		}
+
+		fmt.Println("日本の都市一覧")
+		for _, city := range cities {
+			fmt.Printf("都市名: %s, 人口: %d人\n", city.Name, city.Population)
+
+		}
+	case "add":
+		input_city_country := os.Args[2]
+		input_city_name := os.Args[3]
+		db.Exec(countryCity, input_city_country, input_city_name)
+
+	case "delete":
+		input_city_name := os.Args[2]
+		if err := db.Select(&city, delete_city, input_city_name); errors.Is(err, sql.ErrNoRows) {
+			log.Printf("no such country Name = %s", input_city_name)
+		}
 	}
 
-	fmt.Printf(input_city+"の人口は%d人です\n", city.Population)
 }
